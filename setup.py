c6c6f5fda6959e724f6bbb365817e3fa504da243
Grahame Bowland <grahame@angrygoats.net>
2017-08-22 11:25:51 +0800

    fix up setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@
 from setuptools import setup
 
 setup(
-    name='ckanext-bpatheme',
-    version='0.67.0',
+    name='ckanext-wget',
+    version='0.1.0',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
     author_email='tech@ccg.murdoch.edu.au',
-    url='https://github.com/muccg/ckanext-bpatheme/',
+    url='https://github.com/muccg/ckanext-wget/',
     namespace_packages=['ckanext'],
-    packages=['ckanext.bpatheme'],
+    packages=['ckanext.wget'],
     zip_safe=False,
     include_package_data=True,
-    package_dir={'ckanext.bpatheme': 'ckanext/bpatheme'},
-    package_data={'ckanext.bpatheme': ['*.json', 'templates/*.html', 'templates/*/*.html', 'templates/*/*/*.html', 'static/*.css', 'static/*.png', 'static/*.jpg', 'static/*.css', 'static/*.ico']},
+    package_dir={'ckanext.wget': 'ckanext/wget'},
+    package_data={'ckanext.wget': ['*.json', 'templates/*.html', 'templates/*/*.html', 'templates/*/*/*.html', 'static/*.css', 'static/*.png', 'static/*.jpg', 'static/*.css', 'static/*.ico']},
     entry_points = """
         [ckan.plugins]
-        bpa_theme = ckanext.bpatheme.plugins:CustomTheme
+        wget = ckanext.wget.plugins:WgetPlugin
     """
 )

d0e1a4f584b7f5eec9d0cae3e78688c3b9c6bc51
Grahame Bowland <grahame@angrygoats.net>
2017-08-25 20:51:57 +0800

    wget: generate shell-script, make executable, set auth_tkt header

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # ckanext-wget
 
-This CKAN extension adds functionality to export a `wget` compatible list of download
-links from any CKAN search result package. This list of links includes all resources in
-all packages matching the search.
+This CKAN extension adds a bulk-download facility to CKAN.
 
+Links can be added to organization search packages, as well
+as to the dataset page, which produce a Zip file containing:
+
+  - a list of all relevant resource URLs
+  - a MD5 checksum file
+  - a shell script which downloads those resources with `wget`, then confirms the checksums match
--- ckanext/wget/controller.py
+++ ckanext/wget/controller.py
@@ -1,15 +1,16 @@
-import re
 import datetime
 import logging
 import ckan.plugins as p
 import ckan.lib.helpers as h
+import pipes
 from pylons import config
 from ckan import model
 from ckan.lib.base import abort, BaseController
 from ckan.controllers.organization import OrganizationController
 from ckan.common import request, response, c
 from ckan.logic import NotFound, NotAuthorized, get_action
-from zipfile import ZipFile, ZIP_DEFLATED
+from urlparse import urlparse
+from zipfile import ZipFile, ZipInfo, ZIP_DEFLATED
 from io import BytesIO
 
 _ = p.toolkit._
@@ -39,24 +40,56 @@ and then checksum then. This should operate correctly on any
 Linux distribution, so long as 'wget' and 'md5sum' are installed.
 '''
 
+DOWNLOAD_SCRIPT = '''\
+#!/bin/sh
+
+echo "Downloading data"
+wget %(wget_args)s -c -t 0 -i urls.txt
+
+echo "Data download complete. Verifying checksums:"
+md5sum -c md5sum.txt 2>&1 | tee md5sums.log
+'''
+
 
 def get_timestamp():
     return datetime.datetime.now(
         h.get_display_timezone()).strftime('%Y-%m-%dT%H:%M:%S.%f%z')
 
 
-def bulk_download_zip(title, pfx, urls):
+def bulk_download_zip(pfx, title, resource_iter):
     def ip(s):
         return pfx + '/' + s
 
+    wget_args = []
+    auth_tkt = request.cookies.get('auth_tkt')
+    if auth_tkt is not None:
+        wget_args.append('--header=Cookie: auth_tkt="%s"' % auth_tkt)
+
+    urls = []
+    md5sums = []
+
+    for resource in resource_iter:
+        url = resource['url']
+        urls.append(resource['url'])
+        if 'md5' in resource:
+            filename = urlparse(url).path.split('/')[-1]
+            md5sums.append((resource['md5'], filename))
+
     response.headers['Content-Type'] = 'application/zip'
+    response.headers['Content-Disposition'] = str('attachment; filename="%s.zip"' % pfx)
     fd = BytesIO()
     zf = ZipFile(fd, mode='w', compression=ZIP_DEFLATED)
     zf.writestr(ip('README.txt'), WGET_EXPLANATORY_NOTE % {
-
-    });
+        'timestamp': get_timestamp(),
+        'title': title,
+    })
     zf.writestr(ip('urls.txt'), u'\n'.join(urls) + u'\n')
-    zf.writestr(ip('download.sh'), '#!/bin/bash\n\nwget -i urls.txt')
+    zf.writestr(ip('md5sum.txt'), u'\n'.join('%s  %s' % t for t in md5sums))
+    info = ZipInfo(ip('download.sh'))
+    info.external_attr = 0755 << 16L
+    zf.writestr(info, DOWNLOAD_SCRIPT % {
+        'wget_args': str(' '.join(pipes.quote(t) for t in wget_args))
+    })
     zf.close()
     return fd.getvalue()
 
@@ -92,17 +125,17 @@ class WgetOrganizationController(OrganizationController):
             c.group_dict = self._action('group_show')(context, data_dict)
             c.group = context['group']
         except (NotFound, NotAuthorized):
-            raise
             abort(404, _('Group not found'))
 
         self._read(id, self.limit, group_type)
 
-        urls = []
-        for package in c.page.items:
-            for resource in package['resources']:
-                urls.append(resource['url'])
+        def _resource_iter():
+            for package in c.page.items:
+                for resource in package['resources']:
+                    yield resource
 
-        return bulk_download_zip(urls)
+        name = c.group_dict['name']
+        return bulk_download_zip(name, 'Organization: %s' % (name,), _resource_iter())
 
 
 class WgetPackageController(BaseController):
@@ -131,8 +164,9 @@ class WgetPackageController(BaseController):
         except (NotFound, NotAuthorized):
             abort(404, _('Dataset not found'))
 
-        urls = []
-        for resource in pkg_dict['resources']:
-            urls.append(resource['url'])
+        def _resource_iter():
+            for resource in pkg_dict['resources']:
+                yield resource
 
-        return bulk_download_zip(urls)
+        name = pkg_dict['name']
+        return bulk_download_zip(name, 'Dataset: %s' % (name,), pkg_dict['resources'])

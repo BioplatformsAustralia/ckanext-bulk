d76d8db7392becf8ea82dc1925638c98535663be
Grahame Bowland <grahame@angrygoats.net>
2017-09-27 16:00:55 +0800

    release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bulk',
-    version='1.0.2',
+    version='1.0.3',
     description='',
     license='GPL3',
     author='CCG, Murdoch University',

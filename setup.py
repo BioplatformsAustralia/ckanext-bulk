316a6fd422413412fc1dbf8c9dd7ccb228696ca9
Mark Tearle <mark.tearle@qcif.edu.au>
2021-03-18 14:41:28 +0800

    Release.

--- setup.py
+++ setup.py
@@ -2,21 +2,33 @@
 from setuptools import setup
 
 setup(
-    name='ckanext-bulk',
-    version='1.2.16',
-    description='',
-    license='GPL3',
-    author='Bioplatforms Australia',
-    author_email='help@bioplatforms.com',
-    url='https://github.com/BioplatformsAustralia/ckanext-bulk/',
-    namespace_packages=['ckanext'],
-    packages=['ckanext.bulk'],
+    name="ckanext-bulk",
+    version="1.2.17",
+    description="",
+    license="GPL3",
+    author="Bioplatforms Australia",
+    author_email="help@bioplatforms.com",
+    url="https://github.com/BioplatformsAustralia/ckanext-bulk/",
+    namespace_packages=["ckanext"],
+    packages=["ckanext.bulk"],
     zip_safe=False,
     include_package_data=True,
-    package_dir={'ckanext.bulk': 'ckanext/bulk'},
-    package_data={'ckanext.bulk': ['*.json', 'templates/*.html', 'templates/*/*.html', 'templates/*/*/*.html', 'static/*.css', 'static/*.png', 'static/*.jpg', 'static/*.css', 'static/*.ico']},
+    package_dir={"ckanext.bulk": "ckanext/bulk"},
+    package_data={
+        "ckanext.bulk": [
+            "*.json",
+            "templates/*.html",
+            "templates/*/*.html",
+            "templates/*/*/*.html",
+            "static/*.css",
+            "static/*.png",
+            "static/*.jpg",
+            "static/*.css",
+            "static/*.ico",
+        ]
+    },
     entry_points="""
         [ckan.plugins]
         bulk = ckanext.bulk.plugins:BulkPlugin
-    """
+    """,
 )

8b58589b694d6ae7c79f37d9bd21ac9bc6f5e14b
Mark Tearle <mark.tearle@qcif.edu.au>
2022-10-23 10:30:20 +0800

    Release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="ckanext-bulk",
-    version="1.4.1",
+    version="1.5.0",
     description="",
     license="GPL3",
     author="Bioplatforms Australia",

d1bb7e719ea5ef1a2a17a3dde91801038d2c94e9
Brigette Gonch <b.gonch@qcif.edu.au>
2023-03-15 10:06:49 +1000

    Fix bug with package bulk download

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="ckanext-bulk",
-    version="1.5.6",
+    version="1.5.7",
     description="",
     license="GPL3",
     author="Bioplatforms Australia",
